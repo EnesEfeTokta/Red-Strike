b7807be1ce031b00bebaa2d669ce21b1691e6a85
Enes Efe Tokta <enesefetokta009@gmail.com>
2025-11-26 11:54:28 +0300

    Add initial ammunition system with basic bullet
    Introduces a modular ammunition system including ScriptableObject definitions for ammunition types, a database, and a MonoBehaviour for ammunition instances. Adds a sample 'BasicBullet' prefab, script, and asset to demonstrate the system in use.

---
+++ Red Strike/Assets/AmmunitionSystem/Ammunition.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace AmmunitionSystem
+{
+    [CreateAssetMenu(fileName = "New Ammunition", menuName = "Ammunition System/Ammunition")]
+    public class Ammunition : ScriptableObject
+    {
+        public AmmunitionType ammunitionType;
+        public string ammunitionName;
+        public float damage;
+        public float speed;
+        public float range;
+    }
+}
